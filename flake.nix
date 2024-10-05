4c0bf84650ff7a4123db889126fc7e856935431e
Paresh Adhia <padhia@gmail.com>
2024-10-05 18:05:37 -0400

    added pyDevShell

--- flake.nix
+++ flake.nix
@@ -1,20 +1,35 @@
 {
-  description        = "Library of Nix utility functions";
+  description = "Library of Nix utility functions";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
   let
+    inherit (nixpkgs.lib) genAttrs;
+
     systems = [
       "x86_64-linux"
       "aarch64-linux"
       "x86_64-darwin"
       "aarch64-darwin"
     ];
-    forAllSystems = fn: nixpkgs.lib.genAttrs systems (system: fn nixpkgs.legacyPackages.${system});
+
+    forAllSystems = args: fn:
+      let
+        forEachSystem = system:
+          let
+            pkgs = import nixpkgs ({
+              inherit system;
+              config.allowUnfree = true;
+            } // args);
+          in
+            fn pkgs;
+      in
+        genAttrs systems forEachSystem;
   in {
     lib = {
       inherit forAllSystems;
       mkPyFlake = (import ./mkPyFlake.nix) { inherit nixpkgs forAllSystems; };
+      pyDevShell = import ./pyDevShell.nix;
     };
   };
 }
